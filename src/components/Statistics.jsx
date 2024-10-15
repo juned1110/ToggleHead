325de3c3ab0f829c78f3a450d5c46d3de99376db
juned1110 <junedking1110@gmail.com>
2024-10-15 09:36:25 +0530

    responsive in mobile view

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -20,15 +20,17 @@ const stats = [
 ];
 
 const Statistics = () => (
-  <div className="p-8 w-[90vw] ml-10 bg-[#F6F6F6] grid grid-cols-1 md:grid-cols-4 text-center ">
+  <div className="p-4 md:p-8 w-full md:w-[90vw] mx-auto bg-[#F6F6F6] grid grid-cols-1 md:grid-cols-4 gap-4 text-center">
     {stats.map((stat, index) => (
-      <div key={index} className="bg-[#F6F6F6] p-4 rounded w-[20vw]">
-        <p className="mt-2 -ml-16 text-primary">{stat.description}</p>
-        <h2 className="text-5xl font-bold -ml-16 text-primary">{stat.label}</h2>
-        <p className="mt-2 font-medium -ml-16 text-primary">
+      <div key={index} className="bg-[#F6F6F6] p-4 rounded w-full md:w-[20vw]">
+        <p className="text-sm md:text-base text-primary">{stat.description}</p>
+        <h2 className="text-3xl md:text-5xl font-bold text-primary">
+          {stat.label}
+        </h2>
+        <p className="text-xs md:text-base text-primary mt-2">
           {stat.description}
         </p>
-        <p className="mt-2 ml-10 text-xs font-medium text-left text-primary">
+        <p className="text-xs text-primary mt-2">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic
           culpa dolore, eum tenetur temporibus eaque esse pariatur dolor rerum.
         </p>
