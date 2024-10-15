1199e83b142b03ba3339200e1f5e2d4255a41d33
juned1110 <junedking1110@gmail.com>
2024-10-15 09:36:17 +0530

    responsive in mobile view

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -41,39 +41,29 @@ const HeroSlider = () => {
 
   return (
     <div
-      className={`relative flex items-center justify-between h-[60vh] rounded-lg overflow-hidden ${slides[currentSlide].bg}`}
+      className={`relative flex flex-col-reverse md:flex-row items-center justify-between h-[40vh] md:h-[60vh] rounded-lg overflow-hidden ${slides[currentSlide].bg}`}
     >
-      <div className="w-1/2 p-8">
-        <h1 className="text-3xl font-bold text-primary ">
+      <div className="w-full md:w-1/2 p-4 md:p-8">
+        <h1 className="text-2xl md:text-3xl font-bold text-primary">
           {slides[currentSlide].title}
         </h1>
-        <p className="mt-4 text-gray-600">{slides[currentSlide].description}</p>
-        <button className="mt-6 px-8 py-3 bg-primary text-white rounded-lg">
+        <p className="mt-2 md:mt-4 text-gray-600">
+          {slides[currentSlide].description}
+        </p>
+        <button className="mt-4 md:mt-6 px-6 py-2 md:px-8 md:py-3 bg-primary text-white rounded-lg">
           {slides[currentSlide].buttonText}
-          <span className="ml-2 absolute mt-[0.2vh] text-2xl">
+          <span className="ml-2 absolute text-lg md:text-2xl mt-[0.2vh]">
             <FaCaretRight />
           </span>
         </button>
       </div>
 
-      <div className="w-1/2 h-screen flex items-center justify-center">
+      <div className="w-full md:w-1/2 h-[40vh] md:h-[60vh] flex items-center justify-center">
         <img
           src={slides[currentSlide].imgSrc}
           alt="Slide graphic"
-          className="max-h-full object-fill h-[60vh]"
+          className="max-h-full object-contain"
         />
-      </div>
-
-      {/* vertical scroll effect */}
-      <div className="absolute right-2 top-1/2 transform -translate-y-1/2 flex flex-col items-center space-y-2">
-        {slides.map((_, index) => (
-          <div
-            key={index}
-            className={`w-3 bg-white rounded-full ${
-              currentSlide === index ? "h-8" : "h-2"
-            } transition-all duration-300`}
-          ></div>
-        ))}
       </div>
     </div>
   );
