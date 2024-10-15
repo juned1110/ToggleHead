c9a5a75b2119646a345467daad0ae3479fef3807
juned1110 <junedking1110@gmail.com>
2024-10-15 09:36:32 +0530

    responsive in mobile view

--- src/components/UpcomingExaminations.jsx
+++ src/components/UpcomingExaminations.jsx
@@ -3,20 +3,19 @@ import { FaCalendarAlt } from "react-icons/fa";
 
 const UpcomingExaminations = () => {
   return (
-    <div className="max-w-7xl mx-auto py-10">
-      <div className="flex items-center justify-between">
-        <div className="text-left ml-5">
+    <div className="max-w-7xl mx-auto py-10 px-4">
+      <div className="flex flex-col md:flex-row items-start md:items-center justify-between">
+        <div className="text-left">
           <h2 className="text-2xl font-bold text-primary">
             Upcoming Examinations
           </h2>
-          <p className="text-gray-600 w-[20vw]">
+          <p className="text-gray-600 mt-2 md:mt-0">
             Enquire about the examination & register for the exams
           </p>
         </div>
-        <div className="border-r h-24 mx-4"></div>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mr-[20vw] mt-5">
-          <div className="text-center mt-5">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8 md:mt-0">
+          <div className="text-center">
             <FaCalendarAlt className="text-3xl text-primary mx-auto mb-2" />
             <h3 className="text-lg font-bold text-primary">
               02th October 2014
@@ -24,15 +23,13 @@ const UpcomingExaminations = () => {
             <p className="text-gray-700">Level 1 exam</p>
           </div>
 
-          <div className="text-center mt-5">
+          <div className="text-center">
             <FaCalendarAlt className="text-3xl text-primary mx-auto mb-2" />
             <h3 className="text-lg font-bold text-primary">Nov-Dec 2016</h3>
-            <p className="text-gray-700 text-left ml-8">Level 2</p>
-            <p className="text-gray-700">Lorem ipsum</p>
-            <p className="text-gray-700">Lorem ipsum</p>
+            <p className="text-gray-700">Level 2</p>
           </div>
 
-          <div className="text-center mt-5">
+          <div className="text-center">
             <FaCalendarAlt className="text-3xl text-primary mx-auto mb-2" />
             <h3 className="text-lg font-bold text-primary">
               Ongoing this year
