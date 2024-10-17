326419d0931c5b06e6c7896390b6adb7c4ae88c8
juned1110 <junedking1110@gmail.com>
2024-10-17 14:26:46 +0530

    bug fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-red-900 text-white p-4 flex justify-between items-center">
+    <header className="bg-primary text-white p-4 flex justify-between items-center">
       <div className="text-xl font-bold md:ml-10">LOGO</div>
 
       {/* Hamburger Menu */}
